e64145d2053522b2657f5275f069de9b1844ba81
Micael Rodrigues do Espirito Santo <66914500+mresanto@users.noreply.github.com>
2021-03-17 15:18:12 -0300

    Tela_Usuario

--- app/src/main/java/com/example/osu/MainActivity.java
+++ app/src/main/java/com/example/osu/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.osu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnUsuario, btnMapa, btnScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnUsuario = findViewById(R.id.btnUsuario);
+        btnMapa = findViewById(R.id.btnMapa);
+        btnScore = findViewById(R.id.btnScore);
+
+        btnUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OsuUsuario.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
