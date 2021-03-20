7b7fe3ef140d9911e4b3a933ed47621243e00a5f
Micael Rodrigues do Espirito Santo <66914500+mresanto@users.noreply.github.com>
2021-03-20 14:53:46 -0300

    Telas

---
+++ app/src/main/java/com/example/osu/OsuScore.java
@@ -0,0 +1,14 @@
+package com.example.osu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OsuScore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_osu_score);
+    }
+}
