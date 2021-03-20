a7668e9236a8e9fc220d4b08e589277705979cc2
Leofoli <Leoliveirafe@protonmail.com>
2021-03-20 17:07:19 -0300

    Classe Usuario e começo da integração com a api

--- app/src/main/java/com/example/osu/Classes/Usuario.java
+++ app/src/main/java/com/example/osu/Classes/Usuario.java
@@ -4,4 +4,14 @@ public class Usuario {
 
     public int userId;
     public String username;
+    public String pp_rank;
+    public int level;
+    public int playcount;
+    public String accuracy;
+    public int count_rank_ss;
+    public int count_rank_ssh;
+    public int count_rank_s;
+    public int count_rank_sh;
+    public int count_rank_a;
+
 }
--- app/src/main/java/com/example/osu/OsuUsuario.java
+++ app/src/main/java/com/example/osu/OsuUsuario.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.net.HttpURLConnection;
+import java.net.*;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
 public class OsuUsuario extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_osu_usuario);
+
     }
 }
