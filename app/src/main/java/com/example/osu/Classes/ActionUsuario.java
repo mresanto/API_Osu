8bff518d7ff9230e2417e24d1e16d40eb7b9a349
Micael Rodrigues do Espirito Santo <66914500+mresanto@users.noreply.github.com>
2021-03-26 14:35:52 -0300

    List String Usu

--- app/src/main/java/com/example/osu/Classes/ActionUsuario.java
+++ app/src/main/java/com/example/osu/Classes/ActionUsuario.java
@@ -29,7 +29,7 @@ public class ActionUsuario extends DBHelper {
         cv.put("Accuracy", usuario.accuracy);
         cv.put("Rank_ss", usuario.count_rank_ss);
         cv.put("Rank_ssh", usuario.count_rank_ssh);
-        cv.put("Rank_s", usuario.count_rank_s);
+        cv.put("Ranks_s", usuario.count_rank_s);
         cv.put("Rank_sh", usuario.count_rank_sh);
         cv.put("Rank_a", usuario.count_rank_a);
 
@@ -40,41 +40,41 @@ public class ActionUsuario extends DBHelper {
             return true;
     }
 
-    public List<Usuario> listarUsuario(){
+    public List<String> listarUsuario(){
         SQLiteDatabase db = getReadableDatabase();
-        List<Usuario> usuarios = new ArrayList<>();
+        List<String> usuarios = new ArrayList<>();
         String sql = "SELECT * FROM Usuario ;";
         Cursor c =db.rawQuery(sql, null);
 
         while(c.moveToNext()) {
 
-            Usuario usuario = new Usuario();
-            String id = c.getString(c.getColumnIndex("User_id"));
-            String usernmae = c.getString(c.getColumnIndex("Username"));
-            String pp_rank = c.getString(c.getColumnIndex("PP_rank"));
-            String level = c.getString(c.getColumnIndex("Level"));
-            String playcount = c.getString(c.getColumnIndex("Playcount"));
-            String accuracy = c.getString(c.getColumnIndex("Accuracy"));
-            String rank_ss = c.getString(c.getColumnIndex("Rank_ss"));
-            String rank_ssh = c.getString(c.getColumnIndex("Rank_ssh"));
-            String rank_s = c.getString(c.getColumnIndex("Rank_s"));
-            String rank_sh = c.getString(c.getColumnIndex("Rank_sh"));
-            String rank_a = c.getString(c.getColumnIndex("Rank_a"));
-
-
-            usuario.setUserId(id);
-            usuario.setUsername(usernmae);
-            usuario.setPp_rank(pp_rank);
-            usuario.setLevel(level);
-            usuario.setPlaycount(playcount);
-            usuario.setAccuracy(accuracy);
-            usuario.setCount_rank_ss(rank_ss);
-            usuario.setCount_rank_sh(rank_sh);
-            usuario.setCount_rank_s(rank_s);
-            usuario.setCount_rank_ssh(rank_ssh);
-            usuario.setCount_rank_a(rank_a);
-
-            usuarios.add(usuario);
+            //Usuario usuario = new Usuario();
+            //String id = c.getString(c.getColumnIndex("User_id"));
+            String username = c.getString(c.getColumnIndex("Username"));
+            //String pp_rank = c.getString(c.getColumnIndex("PP_rank"));
+            //String level = c.getString(c.getColumnIndex("Level"));
+            //String playcount = c.getString(c.getColumnIndex("Playcount"));
+            //String accuracy = c.getString(c.getColumnIndex("Accuracy"));
+            //String rank_ss = c.getString(c.getColumnIndex("Rank_ss"));
+            //String rank_ssh = c.getString(c.getColumnIndex("Rank_ssh"));
+            //String rank_s = c.getString(c.getColumnIndex("Ranks_s"));
+            //String rank_sh = c.getString(c.getColumnIndex("Rank_sh"));
+            //String rank_a = c.getString(c.getColumnIndex("Rank_a"));
+//
+//
+            //usuario.setUserId(id);
+            //usuario.setUsername(usernmae);
+            //usuario.setPp_rank(pp_rank);
+            //usuario.setLevel(level);
+            //usuario.setPlaycount(playcount);
+            //usuario.setAccuracy(accuracy);
+            //usuario.setCount_rank_ss(rank_ss);
+            //usuario.setCount_rank_sh(rank_sh);
+            //usuario.setCount_rank_s(rank_s);
+            //usuario.setCount_rank_ssh(rank_ssh);
+            //usuario.setCount_rank_a(rank_a);
+//
+            usuarios.add(username);
         }
         return usuarios;
     }
